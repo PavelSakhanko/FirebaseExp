40c6b9dbad32e2de7ac571e94d41c77522c4565a
Pavel Sakhanko <iospaveldev@gmail.com>
2021-02-10 21:19:29 +0100

    Initial Commit

---
+++ FirebaseExp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseExp
+//
+//  Created by Pavel Sakhanko on 10.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
